5443b2c3bfdbdcd0e04480c1bb863531886b5c78
onen-touw <anton.granitov123@gmail.com>
2026-05-10 19:16:39 +0300

    LIC and RM

--- src/u_cns/u_cns.h
+++ src/u_cns/u_cns.h
@@ -1,3 +1,21 @@
+/*
+* u_cns is © 2026, Anton Granitov (onen-touw), BSTU Voenmeh
+*
+* u_cns is published and distributed under 
+* the Academic Software License v1.0 (ASL).
+*
+* u_cns is distributed in the hope that it will be useful 
+* for non-commercial academic research, but WITHOUT ANY WARRANTY; without
+* even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+* See the ASL for more details.
+*
+* You should have received a copy of the ASL along with this program; 
+* if not, write to anton.granitov123@gmail.com or https://github.com/onen-touw.  
+* It is also published at LICENSE.md in root folder of this repository.
+*
+* You may contact the original licensor at anton.granitov123@gmail.com or https://github.com/onen-touw.
+*/
+
 #pragma once
 
 #include "impl/u_cns_console.h"
